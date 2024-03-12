332532a3878b487282a8ce74f90251f5bd3a0e83
tomga1 <tomasgarcia003@gmail.com>
2024-03-12 17:25:42 -0300

    segundo commit

---
+++ programa/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa
+{
+    internal class Cliente
+    {
+        public int id { get; set; }
+        public string razonsocial { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public string domicilio { get; set; }
+
+    }
+}
---
+++ programa/articuloNegocio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace programa
+{
+    internal class articuloNegocio
+    {
+
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=proyecto; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "Select Id, Codigo, Descripcion, Proveedor, Stock from dbo.articulos";
+                comando.Connection = conexion;  
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.id = (int)lector["Id"];
+                    aux.codigo = (string)lector["Codigo"];
+                    aux.descripcion = (string)lector["Descripcion"];
+                    aux.proveedor = (string)lector["Proveedor"];
+                    aux.stock = (int)lector["Stock"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { 
+                conexion.Close();   
+            }
+
+        }
+    }
+}
